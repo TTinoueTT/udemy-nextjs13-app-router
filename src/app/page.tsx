07c91ca1518a59ffa401adf90c809e9144012264
t_inoue <t.artwork.ceramica@gmail.com>
2024-03-17 16:27:44 +0900

    Streaming HTML
    - api結果をまたずに、必要なコンポーネントをレンダリングさせる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
 import Image from 'next/image';
-// import { Suspense } from 'react';
+import { Suspense } from 'react';
+import SpinnerServer from '@/app/components/servers/spinner-server';
 import NotesListServer from '@/app/components/servers/notes-list-server';
+import TimerCounterClient from './components/client/timer-counter-client';
 
 export default function Home() {
     return (
         <main>
             <div className="m-10 text-center">
                 <p>Hello World🚀</p>
-                {/* <Suspense fallback={<Spinner color="border-green-500" />}> */}
-
-                <NotesListServer />
-
-                {/* </Suspense> */}
+                <Suspense fallback={<SpinnerServer color="border-green-500" />}>
+                    <NotesListServer />
+                </Suspense>
+                <TimerCounterClient />
                 {/* <TimerCounter />
                 <RefreshBtn /> */}
             </div>
